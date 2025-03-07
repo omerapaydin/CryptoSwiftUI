46fc993292037e94cf40cf999cf871bef2eb1b5a
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-07 17:54:44 +0300

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Ömer on 7.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoSwiftUITests/CryptoSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUITests.swift
+//  CryptoSwiftUITests
+//
+//  Created by Ömer on 7.03.2025.
+//
+
+import Testing
+@testable import CryptoSwiftUI
+
+struct CryptoSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
